da1b5469cc1e63285b7f971687ff29cc403568f7
Conorbmurphy <conorbmurphy@gmail.com>
2016-11-27 13:07:23 -0800

    More polished version

--- code/feature_building.py
+++ code/feature_building.py
@@ -1,194 +1,11 @@
-
-from os import listdir
-import numpy as np
-from scipy.io import loadmat
-import pandas as pd
-# import matplotlib
-# matplotlib.use('Agg')
-# import matplotlib.pyplot as plt
-import multiprocessing
-from scipy import signal
-from scipy.stats import kurtosis, skew, pearsonr, entropy, gaussian_kde
 from itertools import combinations
-
-class Patient(object):
-    def __init__(self, patient_number, path, train=True):
-        self.patient_number = int(patient_number)
-        self.path = path + '/'
-        if train:
-            self.files = self.list_files()
-        else:
-            self.files = self.list_files_test()
-        self.reduced = None
-
-    def list_files(self):
-        '''
-        INPUT: None
-        OUTPUT: None, updates file list in ascending order
-        '''
-        files = []
-        for file in listdir(self.path):
-            files.append(file.replace('.', '_').split('_')[1:3])
-        files = sorted(files, key=lambda num: int(num[0]))
-        return [str(self.patient_number)+'_'+file[0]+'_'+file[1]+'.mat' for file in files]
-
-    def list_files_test(self):
-        '''
-        INPUT: None
-        OUTPUT: None, updates file list in ascending order
-        '''
-        files = []
-        for file in listdir(self.path):
-            files.append(file.replace('.', '_').split('_')[2])
-        files = sorted(files, key=lambda num: int(num))
-        return ['new_'+str(self.patient_number)+'_'+file+'.mat' for file in files]
-
-    def return_labels(self):
-        '''
-        INPUT: none
-        OUTPUT: numpy array of recording number and class
-        '''
-        result = [[int(j[0]), int(j[1][0])] for j in [i.split('_')[1:] for i in self.files]]
-        return np.sort(np.array(result))
-
-
-    def fit_train(self, function, verbose=True):
-        '''
-        INPUT: none
-        OUTPUT: none
-        Fits training data using the path
-        '''
-        l, c = len(self.files), 0
-        if self.patient_number == 1:
-            self.files.remove('1_45_1.mat') # remove corrupted  '1_45_1.mat', which is file 641
-            # files = self.files[642:].append(files)
-        for file in self.files:
-            path = self.path + file
-            new_file = loadmat(path)['dataStruct'][0][0][0]
-            i, clas = file.replace('.', '_').split('_')[1:3]
-            if self.reduced != None:
-                self.reduced = np.append(self.reduced, function(i, clas, new_file), axis=0)
-            else:
-                self.reduced = function(i, clas, new_file)
-            if verbose:
-                print 'Fitting file {} / {} complete: file {}'.format(c, l, file)
-            c += 1
-        if verbose:
-            print "Fit all {} files successfully (skipped one of paitent A's files)".format(c)
-
-    def fit_test(self, function, verbose=True):
-        '''
-        INPUT: none
-        OUTPUT: none
-        Fits test data using the path
-        '''
-        l, c = len(self.files), 0
-        for file in self.files:
-            path = self.path + file
-            new_file = loadmat(path)['dataStruct'][0][0][0]
-            i = file.replace('.', '_').split('_')[2]
-            if self.reduced != None:
-                self.reduced = np.append(self.reduced, function(i, None, new_file), axis=0)
-            else:
-                self.reduced = function(i, None, new_file)
-            if verbose:
-                print 'Fitting file {} / {} complete: file {}'.format(c, l, file)
-            c += 1
-        if verbose:
-            print "Fit all {} files successfully".format(c)
-
-
-    def _reduce_16_1000(self, i, clas, mat):
-        '''
-        INPUT: matrix
-        OUTPUT: returns patient information reduced to 1 channel averaged every 1000 inputs
-        if there are all zero values, it will use the value from the most recent window
-        '''
-        temp_mat = mat.mean(axis=1)
-        start = 0
-        result = []
-        for segment in xrange(1000, temp_mat.shape[0]+1000, 1000):
-            subset = temp_mat[start:segment]
-            subset = subset[subset != 0]
-            if len(subset) > 0:
-                result.append(subset.mean())
-            elif len(result) > 0:
-                result.append(result[-1])
-            else:
-                result.append(0)
-            start = segment+1
-        if clas:
-            result.extend([i, clas])
-        else:
-            result.extend([i])
-        return np.array(result).reshape(1,-1)
-
-    def _reduce_simple_channel_avg(self, i, clas, mat):
-        '''
-        INPUT: matrix
-        OUTPUT: returns each channel average
-        '''
-        temp_mat = list(mat.mean(axis=0))
-        if clas:
-            temp_mat.extend([i, clas])
-        else:
-            temp_mat.extend([i])
-        return np.array(temp_mat).reshape(1,-1)
-
-    def _reduce_complex_channel_avg(self, i, clas, mat):
-        '''
-        INPUT: matrix
-        OUTPUT: returns patient information reduced to 16 channels averaged every 24000 inputs
-        if there are all zero values, it will use the value from the most recent window
-        '''
-        temp_mat = np.copy(mat)
-        start = 0
-        result = np.array([])
-        for segment in xrange(24000, 264000, 24000):
-            subset = temp_mat[start:segment,:]
-            subset = subset[np.all(subset, axis=1)]
-            if len(subset) > 0:
-                result = np.append(result, subset.mean(axis=0))
-            elif len(result) > 0:
-                result = np.append(result, result[-16:])
-            else:
-                result = np.append(result, np.zeros(16))
-            start = segment+1
-        if clas:
-            result = np.append(result, np.array([i, clas]))
-        else:
-            result = np.append(result, i)
-        return result.flatten().reshape(1,-1)
-
-
-        temp_mat = list(mat.mean(axis=0))
-        if clas:
-            temp_mat.extend([i, clas])
-        else:
-            temp_mat.extend([i])
-        return np.array(temp_mat).reshape(1,-1)
-
-    def _save_reduce(self, name):
-        '''
-        INPUT: name of file to be saved, with path if needed
-        OUTPUT: None. Saves file
-        '''
-        df = pd.DataFrame(self.reduced) # done with pandas due to formatting issues
-        df.to_csv(name, index=False)
-
-    def print_stats(self):
-        '''
-        INPUT: None
-        OUTPUT: Basic summar stats on three patients
-        '''
-        print 'Patient A has {} of {} segments as class 1 ({} positive)'.format(150, 1301, float(150)/1301)
-        print 'Patient B has {} of {} segments as class 1 ({} positive)'.format(150, 2346, float(150)/2346)
-        print 'Patient C has {} of {} segments as class 1 ({} positive)'.format(150, 2394, float(150)/2394)
-
-
-
-
-
+import multiprocessing
+import numpy as np
+from os import listdir
+import pandas as pd
+from scipy import signal
+from scipy.io import loadmat
+from scipy.stats import kurtosis, skew, pearsonr, entropy, gaussian_kde
 
 
 class Features(object):
@@ -223,7 +40,6 @@ class Features(object):
         self.entropize()
         self.correlate()
 
-        #self.return_results()
 
     def load_file(self):
         '''
@@ -234,6 +50,7 @@ class Features(object):
         '''
         self.temp_mat = loadmat(self.file_name)['dataStruct'][0][0]
         self.temp_mat2 = self.temp_mat[0][np.all(self.temp_mat[0], axis=1)]
+
 
     def metadata(self):
         '''
@@ -256,6 +73,7 @@ class Features(object):
         else:
         	self.sequence = self.temp_mat[4][0][0] # pulls sequence number (only exists in training set)
 
+
     def channel_means(self):
         '''
         INPUT: None
@@ -277,6 +95,7 @@ class Features(object):
             start = segment+1
         self.means = result.reshape(1,-1)
 
+
     def wavelet_transformation(self):
         '''
         INPUT: None
@@ -285,12 +104,13 @@ class Features(object):
             Ricker wave on each channel, saving the means of the squared result.
             This returns a 1x400 numpy array, 25 values for each channel
         '''
-        freq = self.return_frequencies()
+        freq = self._return_frequencies()
         result = np.array([])
         for i in range(16):
             cwtavg = signal.cwt(self.temp_mat[0][:,i], signal.ricker, freq) # throws error w/ temp_mat2
             result = np.concatenate([result, (cwtavg**2).mean(axis=1)])
         self.wavelets = result.reshape(1,-1)
+
 
     def method_of_moments(self):
         '''
@@ -349,6 +169,7 @@ class Features(object):
         else:
             self.mom = np.zeros(118).reshape(1, -1) # BE CAREFUL WITH THIS
 
+
     def entropize(self):
         '''
        INPUT: None
@@ -367,6 +188,7 @@ class Features(object):
 	except ValueError:
 		self.entropies = np.zeros(16).reshape(1, -1)
 
+
     def correlate(self):
         '''
         INPUT: None
@@ -381,6 +203,7 @@ class Features(object):
         corr_mean = np.mean(correlations)
         corr_var = np.var(correlations)
         self.correlations = np.hstack([np.array(correlations), corr_mean, corr_var]).reshape(1,-1)
+
 
     def return_results(self):
         '''
@@ -418,7 +241,7 @@ class Features(object):
         	return np.ones(742)*-1
 
 
-    def return_frequencies(self):
+    def _return_frequencies(self):
         '''
         INPUT: None
         OUTPUT: numpy array of 25 frequencies spaced 5 each over the common
@@ -437,107 +260,6 @@ class Features(object):
             np.linspace(12, 2, 5)]) # gamma waves
         return frequencies
 
-def reduce_one_file(file_name):
-    '''
-    Reuces one test or train recording by file name
-    '''
-    print 'Fitting {}'.format(file_name)
-    temp_mat = loadmat(file_name)['dataStruct'][0][0]
-
-    patient, id, clas = file_name.replace('.', '_').replace('/','_').split('_')[4:7]
-    contaminated = file_name.split('/')[-1] in label
-    if patient == 'new': # in case of test data
-         patient, id = file_name.replace('.', '_').replace('/','_').split('_')[6:8]
-	 clas = None
-    else:
-	 sequence = temp_mat[4][0][0] # pulls sequence number (only exists in training set)
-
-    start = 0
-    result1 = np.array([])
-    for segment in xrange(24000, 264000, 24000):
-        subset = temp_mat[0][start:segment,:]
-        subset = subset[np.all(subset, axis=1)]
-        if len(subset) > 0:
-            result1 = np.append(result1, subset.mean(axis=0))
-        elif len(result1) > 0:
-            result1 = np.append(result1, result1[-16:])
-        else:
-            result1 = np.append(result1, np.zeros(16))
-        start = segment+1
-    result1 = result1.reshape(1,-1)
-
-    result2 = np.array([])
-    for i in range(16):
-        cwtavg = signal.cwt(temp_mat[0][:,i], signal.ricker, frequencies)
-        result2 = np.concatenate([result2, (cwtavg**2).mean(axis=1)])
-    result2 = result2.reshape(1,-1)
-    #result = np.append(result, result2)
-
-    result3 = kurtosis(temp_mat[0]).reshape(1,-1) # adds kurtosis for each channel
-    result4 = skew(temp_mat[0]).reshape(1,-1) # adds skew for each channel
-
-    temp_mat2 = temp_mat[0][np.all(temp_mat[0], axis=1)]
-    if temp_mat2.shape[0] > 0:
-        arith_mean_channel = temp_mat2.mean(axis=0)
-        arith_mean_total = temp_mat2.mean()
-        variance_channel = temp_mat2.var(axis=0, ddof=1)
-        variance_total = temp_mat2.var(ddof=1)
-        variance_of_variances = variance_channel.var(ddof=1) # this is very high
-        std_channel = temp_mat2.std(axis=0)
-        std_total = temp_mat2.std()
-        max_channel = temp_mat2.max(axis=0)
-        max_total = temp_mat2.max()
-        min_channel = temp_mat2.min(axis=0)
-        min_total = temp_mat2.min()
-        median_channel = np.median(temp_mat2, axis=0)
-        median_total = np.median(temp_mat2)
-
-        entropies = []
-        for col in range(16):
-            kde = gaussian_kde(temp_mat2[:,col])
-            r = np.linspace(min(temp_mat2[:,col]), max(temp_mat2[:,col]),\
-                len(temp_mat2[:,col])/1E3)
-            entropies.append(entropy(kde.evaluate(r)))
-
-        correlations = []
-        for c in combinations(range(16), 2):
-            correlations.append(pearsonr(temp_mat2[c[0]], temp_mat2[c[1]])[0])
-        corr_mean = np.mean(correlations)
-        corr_var = np.var(correlations)
-
-        result_mom = np.hstack(np.array([arith_mean_channel,
-                        arith_mean_total,
-                        variance_channel,
-                        variance_total,
-                        variance_of_variances,
-                        std_channel,
-                        std_total,
-                        max_channel,
-                        max_total,
-                        min_channel,
-                        min_total,
-                        median_channel,
-                        median_total,
-			            np.array(entropies),
-			            np.array(correlations),
-                        corr_mean,
-                        corr_var]).flat).reshape(1,-1)
-    else:
-        result_mom = np.zeros(241).reshape(1, -1) # BE CAREFUL WITH THIS
-
-    try:
-    	if clas:
-		result = np.concatenate([result1, result2, result3, result4, result_mom,
-			np.array([patient, id, sequence, contaminated, clas]).reshape(1,-1)], axis=1)
-    	else:
-        	result = np.concatenate([result1, result2, result3, result4, result_mom,
-                        np.array([patient, id]).reshape(1,-1)], axis=1)
-    	return result.flatten().reshape(1,-1)
-    except ValueError:
-	print 'Unable to process {} due to ValueError, returning negative ones'\
-        .format(file_name)
-	return np.ones(742)*-1
-
 
 def return_labels():
     '''
@@ -549,12 +271,19 @@ def return_labels():
 
 
 def Feature_wrapper(path):
-    ''' 
-    INPUT: file path 
-    OUTPUT: Features result
     '''
-    a = Features(path)
-    return a.return_results()
+    INPUT: file path
+    OUTPUT: Features result
+        In case of error, return all -1's and append path to global list 'errors'
+    '''
+    try:
+        a = Features(path)
+        return a.return_results()
+    except:
+        print 'Unexpected error on {}'.format(path)
+        return (np.ones(821)*-1).reshape(-1,1)
+        errors.append(path)
+
 
 def reduce_parallel():
     '''
@@ -582,5 +311,11 @@ def reduce_parallel():
 
 
 if __name__ == '__main__':
+    errors = []
     label = return_labels()
-    #reduce_parallel()
+    
+    reduce_parallel()
+
+    print 'Errors in fitting {} files'.format(len(errors))
+    for error in errors:
+        print error
